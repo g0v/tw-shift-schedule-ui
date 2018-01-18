438892971d4e3daf6f800e475eac9b85dbbed700
Poga Po <poga.bahamut@gmail.com>
2018-01-19 06:22:59 +0800

    canvas

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,7 +1,7 @@
 
 import moment from 'moment'
 import shift from 'tw-shift-schedule'
-import {Stage, Layer, Rect, Text} from 'react-konva'
+import {Stage, Layer, Rect, Text, Line} from 'react-konva'
 import Konva from 'konva'
 
 var React = require('react')
@@ -14,7 +14,27 @@ class Canvas extends React.Component {
       let m = momentFromItem(item)
       m.type = 'work'
 
-      items.push(m)
+      // 如果跨日就切成兩個
+      if (m.start.day() !== m.end.day()) {
+        let s1 = m.start.clone()
+        let e1 = m.start.clone().endOf('day')
+        items.push({
+          start: s1,
+          end: e1,
+          length: e1.diff(s1, 'minutes'),
+          type: 'work'
+        })
+        let s2 = m.end.clone().startOf('day')
+        let e2 = m.end.clone()
+        items.push({
+          start: s2,
+          end: e2,
+          length: e2.diff(s2, 'minutes'),
+          type: 'work'
+        })
+      } else {
+        items.push(m)
+      }
     }
     return items
   }
@@ -34,22 +54,36 @@ class Canvas extends React.Component {
 
   render () {
     let tokens = this.shiftTokens()
-    let colors = ['#aaaaaa', '#333333']
+    console.log(tokens)
 
     let items = this.listItems()
-    var listItems = items.map((item, index) => {
-      console.log('render', item)
-      let x = Math.floor((item.start.hour() * 60 + item.start.minutes()) / 2)
-      console.log(item.start, items[0].start)
-      let y = item.start.startOf('day').diff(items[0].start.startOf('day'), 'day') * 50 + 20
-      console.log(index, x, y)
+    let listItems = items.map((item, index) => {
+      let x = Math.floor((item.start.hour() * 60 + item.start.minutes()) / 2) + 100
+      let y = item.start.clone().startOf('day').diff(items[0].start.clone().startOf('day'), 'day') * 50 + 20
       return (
         <Rect key={index}
           x={x}
           y={y}
           width={item.length / 2}
           height={50}
-          fill={colors[index % 2]} />
+          onMouseEnter={() => {
+            document.body.style.cursor = 'pointer'
+          }}
+          onMouseLeave={() => {
+            document.body.style.cursor = 'default'
+          }}
+          fill='#cccccc' />
+      )
+    })
+    let listItemlabels = items.map((item, index) => {
+      let x = Math.floor((item.start.hour() * 60 + item.start.minutes()) / 2) + 100
+      let y = item.start.clone().startOf('day').diff(items[0].start.clone().startOf('day'), 'day') * 50 + 20
+      return (
+        <Text key={index}
+          x={x}
+          y={y}
+          text={`${Math.floor(item.length / 60)} 時 ${items.length % 60} 分`}
+          color='black' />
       )
     })
 
@@ -58,18 +92,48 @@ class Canvas extends React.Component {
       grid.push(
         <Text key={i}
           text={`${i}`}
-          x={i * 30}
+          x={i * 30 + 100}
           y={5} />
       )
     }
 
+    let rowCount = (items[items.length - 1].start.clone().startOf('day').diff(items[0].start.clone().startOf('day'), 'day') + 1)
+    let height = rowCount * 50 + 20
+
+    let dates = []
+    for (let i = 0; i < rowCount; i++) {
+      dates.push(
+        <Text key={i}
+          text={items[0].start.clone().startOf('day').add(i, 'day').format('YYYY-MM-DD')}
+          x={10}
+          y={i * 50 + 40} />
+      )
+    }
+    let rowLines = []
+    for (let i = 0; i < rowCount; i++) {
+      rowLines.push(
+        <Line
+          key={i}
+          points={[0, i * 50 + 70, 820, i * 50 + 70]}
+          stroke='#dddddd'
+          />
+      )
+    }
+
     return (
-      <Stage width={720} height={listItems.length * 50 + 20}>
+      <Stage width={820} height={height}>
         <Layer>
           {grid}
         </Layer>
         <Layer>
           {listItems}
+          {listItemlabels}
+        </Layer>
+        <Layer>
+          {dates}
+        </Layer>
+        <Layer>
+          {rowLines}
         </Layer>
       </Stage>
     )
